6d98c298c51c46dc88526b4c18e3faac851c03b2
Nikolai-Raevsky <71659061+Nikolai-Raevsky@users.noreply.github.com>
2021-10-19 12:47:47 -0700

    Transfer IO Test to GitHub
    I don't want to lose my work

---
+++ ConsoleIOandIteration/Project1/main.cpp
@@ -0,0 +1,32 @@
+//-----------------------------------------------------------------------------------------------------------------
+//		Filename: main.cpp
+//		Author: Nikolai Raevsky (nikolai.raevsky@digipen.edu)
+//		Project: PRG 5.2.7 CORE ASSIGNMENT - Console I/O and Iteration in C++
+// Course: WANIC VGP2
+//
+//Copyright © 2021 DigiPen (USA) Corporation.
+//
+//
+//-------------------------------------------------------------------------------------------------------------------
+
+
+//Includes
+#include <iostream>
+
+int main(void)
+{
+	int num;
+	int nextnum = 0;
+	std::cout << "Hello world. \n";
+	std::cout << "Gimme two integers or else! :) ";
+	std::cin >> num >> nextnum;
+	;
+	std::cout << "\nNum is " << num << " and nextnum is " << nextnum;
+
+
+
+	//Rectangle tests
+
+
+	//Triangle tests
+}
