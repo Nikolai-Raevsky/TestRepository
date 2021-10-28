da39dae8afedbd3a9d6c6ccd46ec9e7bc0a6e44c
Nikolai-Raevsky <71659061+Nikolai-Raevsky@users.noreply.github.com>
2021-10-28 14:04:43 -0700

    Copied project files and made board.cpp

---
+++ Tic-Tac-Toe/TicTacToe Student/Board.cpp
@@ -0,0 +1,8 @@
+/**********************************************
+*	
+*	File Name: Board.cpp
+*	Name: Nikolai Raevsky (nikolai.raevsky@digipen.edu)
+*	Project: PRG 5.5.6 CORE Assignment - Tic-Tac-Toe
+*	Course: WANIC VGP2
+*	Copyright © DigiPen (USA) Corporation. 
+* ********************************************/
