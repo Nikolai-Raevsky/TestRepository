d601002235ec2273df1f1e08808810e3730f4691
Nikolai-Raevsky <71659061+Nikolai-Raevsky@users.noreply.github.com>
2021-10-28 14:40:57 -0700

    Made this into a project and started BoardCreate imp

--- Tic-Tac-Toe/TicTacToe Student/Board.cpp
+++ Tic-Tac-Toe/TicTacToe Student/Board.cpp
@@ -6,3 +6,47 @@
 *	Course: WANIC VGP2
 *	Copyright © DigiPen (USA) Corporation. 
 * ********************************************/
+
+//Includes
+#include <iostream>
+#include "Board.h"
+namespace CS170
+{
+
+
+	struct Board
+	{
+		TileState data[3][3];
+	};
+
+	Board* BoardCreate()
+	{
+		Board* board = new Board;
+		return board;
+	}
+
+	void BoardFree(Board*& theBoard)
+	{
+
+	}
+
+	void BoardDisplay(const Board& board)
+	{
+
+	}
+
+	PlaceResult BoardPlaceToken(Board& board, unsigned row, unsigned column, TileState value)
+	{
+		
+	}
+
+	void BoardReset(Board& board)
+	{
+
+	}
+
+	BoardState BoardGetState(const Board& board)
+	{
+
+	}
+}
