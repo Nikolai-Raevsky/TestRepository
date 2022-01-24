232b77ef075aa17f00d6963a45e334f5b6d115d3
Nikolai-Raevsky <71659061+Nikolai-Raevsky@users.noreply.github.com>
2022-01-24 14:18:36 -0800

    Start Vector2D Project

---
+++ Vector2D/Vector2D Student/Vector2D.cpp
@@ -0,0 +1,35 @@
+#include "Vector2D.h"  // Vector2D members
+#include <cmath>    // sqrt, atan, sin, cos
+
+namespace CS170
+{
+
+///////////////////////////////////////////////////////////////////////////////
+// 22 public member functions (2 constructors, 2 accessors, 2 mutators, 7 others, 9 operators) 
+
+// Constructors (2)
+Vector2D::Vector2D(float x_, float y_)
+  : x(x_), y(y_)
+{
+}
+
+// Accessors (2)
+
+
+// Mutators (2)
+
+
+// Other functions (7)
+
+
+// Operators (9)
+
+
+///////////////////////////////////////////////////////////////////////////////
+// 2 non-member, non-friend functions (operators)
+
+
+} // namespace CS170
+
+
+
---
+++ Vector2D/Vector2D Student/Vector2D.h
@@ -0,0 +1,39 @@
+////////////////////////////////////////////////////////////////////////////////
+#ifndef VECTOR2D_H
+#define VECTOR2D_H
+////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream> // istream, ostream
+
+namespace CS170
+{
+	class Vector2D
+	{
+	public:
+		// Constructors (2)
+		Vector2D(float x, float y);
+		
+		// Accessors (2)
+		
+		
+		// Mutators (2)
+		
+		
+		// Other functions (7)
+		
+
+		// Overloaded operators (9 member functions)
+		
+		
+	private:
+		float x; // The x-coordinate of a Vector2D
+		float y; // The y-coordinate of a Vector2D
+	};
+  
+	// Overloaded operators (2 non-member, non-friend functions)
+	
+
+} // namespace CS170
+
+#endif
+////////////////////////////////////////////////////////////////////////////////
