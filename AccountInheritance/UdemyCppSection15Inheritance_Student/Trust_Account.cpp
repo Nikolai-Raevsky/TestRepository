2c71b4578e24c50e1eba27be7d2e1c8e7e781141
Nikolai-Raevsky <71659061+Nikolai-Raevsky@users.noreply.github.com>
2022-02-09 14:53:04 -0800

    Add deposit function

--- AccountInheritance/UdemyCppSection15Inheritance_Student/Trust_Account.cpp
+++ AccountInheritance/UdemyCppSection15Inheritance_Student/Trust_Account.cpp
@@ -21,4 +21,21 @@ std::ostream& operator<< (std::ostream& os, const Trust_Account& account)
 	return os;
 }
 
+Trust_Account::Trust_Account(std::string name, double balance, double int_rate) 
+	: Account{ name, balance }, int_rate{ int_rate }
+{
+
+}
+
+bool Trust_Account::deposit(double amount)
+{
+	amount += amount * (int_rate / 100);
+	return Account::deposit(amount);
+}
+
+bool Trust_Account::withdraw(double amount)
+{
+
+}
+
 
