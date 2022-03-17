81dd1ad1af99eb89eb860a3d54b1efea26ae717e
Nikolai-Raevsky <71659061+Nikolai-Raevsky@users.noreply.github.com>
2022-03-17 13:36:07 -0700

    I got the inheritance project to work finally

--- AccountInheritance/UdemyCppSection15Inheritance_Student/Checking_Account.h
+++ AccountInheritance/UdemyCppSection15Inheritance_Student/Checking_Account.h
@@ -12,10 +12,11 @@
 
 //Checking Account is an account with a name and a balance. 
 // Checking_Account constructor constructs a checking account with a name and a balance that gets initialized into the Account parent class
-// withdraw method deducts a $1.50 fee from the balance and subtracts the amount parameter from the account's balance
-// << overload outputs the account's name and balance to the console
+
+
 //
 class Checking_Account : public Account {
+	// << overload outputs the account's name and balance to the console
 	friend std::ostream &operator<<(std::ostream& os, const Checking_Account& account);
 private:
 	static constexpr const char* def_name = "Unnamed Checking Account";
@@ -23,10 +24,14 @@ private:
 	
 
 public:
-	
+	// Checking_Account constructor constructs a checking account with a name and a balance that gets initialized into the Account parent class
 	Checking_Account(std::string name = def_name, double balance = def_balance);
 	
-	
+	// withdraw method deducts a $1.50 fee from the balance and subtracts the amount parameter from the account's balance
+	//Params:
+	//	amount: a double that will be subtracted from the user's account
+	//Returns:
+	//	a bool that will be used in the account util withdraw function to determine what message to print
 	bool withdraw(double amount);
 	//We're going to steal the deposit method from the parent class and nobody will stop us
 };
