14b083952619c728f52102e74b1f49085316de6c
Nikolai-Raevsky <71659061+Nikolai-Raevsky@users.noreply.github.com>
2022-03-31 13:50:45 -0700

    Got new project and started work on player controller

--- GhostGoblinsEnemyRemake/Assets/Scripts/ArthurController.cs
+++ GhostGoblinsEnemyRemake/Assets/Scripts/ArthurController.cs
@@ -12,10 +12,11 @@ using UnityEngine;
 public class ArthurController : MonoBehaviour
 {
     public float moveSpeed = .5f;
+    Rigidbody2D myRB;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRB = GetComponent<Rigidbody2D>();   
     }
 
     // Update is called once per frame
@@ -36,19 +37,30 @@ public class ArthurController : MonoBehaviour
     private void FixedUpdate()
     {
         ////Horizontal movement //TOMORROW REMAKE IT W/ PHYSICS TO GET MORE CONTROLLER CUSTOMIZERS!! OR FIGURE OUT HOW TO MAKE PARABOLA JUMP ARC
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey("right"))
         {
-            transform.position = new Vector3(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+            //transform.position = new Vector3(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+            myRB.velocity = new Vector2(moveSpeed, myRB.velocity.y);
+            Debug.Log("Right");
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey("left"))
         {
-            transform.position = new Vector3(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+            //transform.position = new Vector3(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+            myRB.velocity = new Vector2(-moveSpeed, myRB.velocity.y);
+            Debug.Log("Left");
         }
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey("space"))
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y + moveSpeed * Time.deltaTime, transform.position.z);
+            //transform.position = new Vector3(transform.position.x, transform.position.y + moveSpeed * Time.deltaTime, transform.position.z);
+            myRB.velocity = new Vector2(0, moveSpeed);
+            Debug.Log("Space");
+        }
+
+        else
+        {
+            myRB.velocity = new Vector2(0, 0);
         }
     }
 }
