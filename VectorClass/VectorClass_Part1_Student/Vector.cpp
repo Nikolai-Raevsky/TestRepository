2ac188ba4116d117363911fdc34586588a36fad8
Nikolai-Raevsky <71659061+Nikolai-Raevsky@users.noreply.github.com>
2022-04-11 14:55:10 -0700

    Documentation and started grow

--- VectorClass/VectorClass_Part1_Student/Vector.cpp
+++ VectorClass/VectorClass_Part1_Student/Vector.cpp
@@ -1,3 +1,12 @@
+////////////////////////////////////////
+// Author: Nikolai Raevsky (nikolai.raevsky@digipen.edu)
+// Instructor: Jeremy Kings (jeremy.kings@digipen.edu)
+// Assignment:PRG 8.1.1 CORE ASSIGNMENT: Vector Container Class Part 1
+// Course: WANIC VGP2
+////////////////////////////////////////
+
+
+
 #include <iostream>  // cout
 #include <cstdlib>   // abort
 #include "Vector.h"
@@ -39,15 +48,19 @@ void Vector::clear(void)
     delete[] array_;
     array_ = nullptr;
     size_ = 0;
+    capacity_ = 0;
+    
 }
 
 int Vector::operator[](unsigned index) const
 {
+    check_bounds(index);
     return array_[index];
 }
 
 int& Vector::operator[](unsigned index)
 {
+    check_bounds(index);
     return array_[index];
 }
 
@@ -76,7 +89,20 @@ void Vector::grow(void)
   capacity_ = (capacity_) ? capacity_ * 2 : 1;
     
     // Rest of the code here...
+    
 
+  //Instructions say allocate another int array that's twice the size of the OG array  (new int* array_ = (sizeof(int)*capacity))?
+  int* array_Copy = new int(capacity_);
+  //Loop through the og array and copy each of the elements into the new array
+  for (unsigned i = 0; i < size_; i++)
+  {
+      array_Copy[i] = array_[i];
+  }
+  //Free the memory of the og array
+  delete array_;
+  array_ = nullptr;
+  //increment allocs_
+  allocs_ += 1;
 }
 
 } // namespace CS170
