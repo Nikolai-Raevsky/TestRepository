d44031019b2bd9281c2b04400c1551979d9745be
Nikolai-Raevsky <71659061+Nikolai-Raevsky@users.noreply.github.com>
2022-04-15 13:53:14 -0700

    Working on pop_front but finished copy constructor and assignment overload

--- VectorClass/VectorClass_Part2_Student/VectorClassPart2/VectorClassPart2/Vector.cpp
+++ VectorClass/VectorClass_Part2_Student/VectorClassPart2/VectorClassPart2/Vector.cpp
@@ -22,6 +22,15 @@ Vector::Vector(const int array[], unsigned size) : size_(size), capacity_(size),
 {
     *array_ = *array;
 }
+
+Vector::Vector(const Vector& rhs) : size_(rhs.size()), capacity_(rhs.capacity()), allocs_(rhs.allocations())
+{
+    array_ = new int(capacity_);
+    for (unsigned i = 0; i < rhs.size_; i++)
+    {
+        array_[i] = rhs.array_[i];
+    }
+}
 Vector::~Vector(void)
 {
     clear();
@@ -58,27 +67,13 @@ unsigned Vector::allocations(void) const
   return allocs_;
 }
 
-
-
-int Vector::operator[](unsigned index) const
-{
-    check_bounds(index);
-    return array_[index];
-}
-
-int& Vector::operator[](unsigned index)
-{
-    check_bounds(index);
-    return array_[index];
-}
-
 void Vector::push_back(int value)
 {
-    
+
     if (size_ == capacity_)
     {
         grow();
-        
+
     }
 
     array_[size_] = value;
@@ -93,7 +88,7 @@ void Vector::push_front(int value)
     }
 
     //I diffed the output from this implementation against the master output and the files were identical even though I'm confused about the pseudocode of this. Isn't array_[size_-1] the left index to the first element that the array goes through in the array? How does this implementation shift the elements rightwards?
-    unsigned rightIndex = size_-1;
+    unsigned rightIndex = size_ - 1;
     for (unsigned i = size_; i > 0; i--)
     {
         array_[i] = array_[rightIndex];
@@ -106,14 +101,39 @@ void Vector::push_front(int value)
 
 void Vector::pop_back(void)
 {
-    if (array_[size_] != NULL)
+    size_ -= 1;
+    
+}
+
+void Vector::pop_front(void)
+{
+    unsigned rightIndex = size_ - 1;
+    for (unsigned i = 0; i < size_; i++)
     {
-        array_[size_] = NULL;
+        array_[i] = array_[rightIndex];
+        rightIndex += 1;
     }
+    size_ -= 1;
+
     return;
 }
 
 //Operators
+int Vector::operator[](unsigned index) const
+{
+    check_bounds(index);
+    return array_[index];
+}
+
+int& Vector::operator[](unsigned index)
+{
+    check_bounds(index);
+    return array_[index];
+}
+
+
+
+
 
 Vector& Vector::operator+=(const Vector& rhs)
 {
@@ -132,6 +152,21 @@ Vector Vector::operator+(const Vector& rhs) const
     newVector +=  *this;
     newVector += rhs;
     return newVector;
+}
+
+Vector& Vector::operator=(const Vector& rhs) 
+{
+    size_ = rhs.size();
+    capacity_ = rhs.capacity();
+    allocs_ = rhs.allocations();
+
+    for (unsigned i = 0; i < size_; i++)
+    {
+        push_back(rhs[i]);
+    }
+
+    return *this;
+    
 }
 
 
