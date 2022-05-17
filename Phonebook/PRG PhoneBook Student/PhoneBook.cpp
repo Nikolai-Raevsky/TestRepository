1b578c408ad2c3ca88316e63df34b277ae890725
Nikolai-Raevsky <71659061+Nikolai-Raevsky@users.noreply.github.com>
2022-05-17 13:25:48 -0700

    Update PhoneBook.cpp

--- Phonebook/PRG PhoneBook Student/PhoneBook.cpp
+++ Phonebook/PRG PhoneBook Student/PhoneBook.cpp
@@ -70,12 +70,12 @@ PhoneBook::PhoneBook(std::string filename_)
 
 	 
 
-	//Once all the phone entry information is collected add it to phoneentry vector in phonebook
+	
 }
 
 PhoneBook::PhoneBook(const PhoneBook& copy_)
 {
-	//For this one, I'll need iterators I think
+	
 
 	for (auto iter = copy_.entries.begin(); iter < copy_.entries.end(); ++iter)
 	{
@@ -93,45 +93,31 @@ PhoneBook& PhoneBook::operator=(const PhoneBook& rhs)
 	{
 		AddEntry((*iter)->name, (*iter)->last_name, (*iter)->email, (*iter)->region, (*iter)->number);
 	}
-	//change this later
+	
 	return *this;
 }
 
-//Description: Outputs this PhoneBook to a file
-//Params:
-//	filename_: the name of the file the user wants to output to
+
 void PhoneBook::Save(std::string filename_) const
 {
 	std::ofstream outputFile(filename_);
 	outputFile << *this;
 
 }
-//Description:
-//	Adds a new PhoneEntry to the entries vector of PhoneBook
-//Params:
-//	name_ : This will be the name member of the new PhoneEntry
-//	last_name_ : This will be the last_name member of the new PhoneEntry
-//	eamil_ : This will be the email member of the new PhoneEntry
-//	region_ : This will be the region member of the new PhoneEntry
-//	number_ : This will be the number member of the new entry
+
 void PhoneBook::AddEntry(const std::string& name_, const std::string& last_name_, const std::string& email_, int region_, int number_)
 {
 	PhoneEntry* newEntry = new PhoneEntry(name_, last_name_, email_, region_, number_);
 	entries.push_back(newEntry);
 }
 
-//Description:
-//	sorts the entries based on how early their names are in alphabetical order
+
 void PhoneBook::SortByName()
 {
 	//I need a comparing function and std::sort for this one
 	std::sort(entries.begin(), entries.end(), PhoneEntryCompare);
 }
-//Description:
-//	Removes a PhoneEntry from the entries vector based on whether their name member contains a certain sequence of characters 
-// (in most uses it would be whole names but in the driver we can remove entries based on sequences of as few as two characters)
-//Params:
-//	subString_ : The character sequence that the name must feature to be removed from entries
+
 void PhoneBook::RemoveEntriesByName(std::string subString_)
 {
 	//This function uses entries->name.substr(pos) and entries->name.find("on"), a for loop
