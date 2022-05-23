3372c6b331350bcbd7e2d82eabed477ce750d77c
Nikolai-Raevsky <71659061+Nikolai-Raevsky@users.noreply.github.com>
2022-05-23 13:53:16 -0700

    Build and make more changes

--- GhostGoblinsEnemyRemake/Assets/Scripts/ArthurController.cs
+++ GhostGoblinsEnemyRemake/Assets/Scripts/ArthurController.cs
@@ -14,6 +14,7 @@ public class ArthurController : MonoBehaviour
     public float moveSpeed = .5f;
     public float jumpModifier = .5f;
     Rigidbody2D myRB;
+    bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
@@ -38,31 +39,47 @@ public class ArthurController : MonoBehaviour
     private void FixedUpdate()
     {
         ////Horizontal movement  OR FIGURE OUT HOW TO MAKE PARABOLA JUMP ARC
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey("right"))
         {
             //transform.position = new Vector3(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
-            myRB.velocity = new Vector2(moveSpeed*Time.deltaTime, myRB.velocity.y);
+            myRB.velocity = new Vector2(moveSpeed, myRB.velocity.y);
+            Debug.Log("right");
             
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        //if (Input.GetKeyUp(KeyCode.RightArrow))
+        //{
+        //    myRB.velocity = new Vector2(0, 0);
+        //    Debug.Log("The release of all keys is triggered and the velocity is (0,0)");
+        //}
+
+        else if (Input.GetKey("left"))
         {
             //transform.position = new Vector3(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
-            myRB.velocity = new Vector2(-moveSpeed* Time.deltaTime, myRB.velocity.y);
+            myRB.velocity = new Vector2(-moveSpeed, myRB.velocity.y);
+            Debug.Log("left");
             
         }
 
-        if (Input.GetKey(KeyCode.Space))
+        //if (Input.GetKeyUp(KeyCode.LeftArrow))
+        //{
+        //    myRB.velocity = new Vector2(0, 0);
+        //    Debug.Log("The release of all keys is triggered and the velocity is (0,0)");
+        //}
+        else
+        {
+            myRB.velocity = new Vector2(0, myRB.velocity.y);
+        }
+        if (Input.GetKey("space"))
         {
             //transform.position = new Vector3(transform.position.x, transform.position.y + moveSpeed * Time.deltaTime, transform.position.z);
-            myRB.velocity = new Vector2(myRB.velocity.x, jumpModifier * Time.deltaTime);
+            myRB.velocity = new Vector2(myRB.velocity.x, jumpModifier);
             //Tomorrow make independent jump height variable
             Debug.Log("Space");
+            isGrounded = false;
         }
-
-        if (Input.GetKey(KeyCode.None))
-        {
-            myRB.velocity = new Vector2(0, 0);
-        }
+        
+        //Stopping the momentum has utmost priority
+        
     }
 }
--- GhostGoblinsEnemyRemake/Assets/Scripts/ArthurLockedJump.cs
+++ GhostGoblinsEnemyRemake/Assets/Scripts/ArthurLockedJump.cs
@@ -1,19 +1,69 @@
-//A locked jump arc script
+//-------------------------
+//Name: Nikolai Raevsky (nikolai.raevsky@digipen.edu)
+//Date: 5/20/2022
+//Description: A player controlling script with a locked jump arc. This is an arc that is unchangeable after initiation in the vein of the Castlevania 1 jump or the Ghosts n Goblins jump
+//-------------------------
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ArthurLockedJump : MonoBehaviour
 {
+    public float moveSpeed = .5f;
+    public float jumpModifier = .5f;
+    private Vector3 lastMotion;
+    Rigidbody2D myRB;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ////Horizontal movement
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    transform.position = new Vector3(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+        //}
+
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    transform.position = new Vector3(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+        //}
+    }
+
+    private void FixedUpdate()
+    {
+        ////Horizontal movement  OR FIGURE OUT HOW TO MAKE PARABOLA JUMP ARC
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //transform.position = new Vector3(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+            myRB.velocity = new Vector2(moveSpeed * Time.deltaTime, myRB.velocity.y);
+
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //transform.position = new Vector3(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+            myRB.velocity = new Vector2(-moveSpeed * Time.deltaTime, myRB.velocity.y);
+
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //transform.position = new Vector3(transform.position.x, transform.position.y + moveSpeed * Time.deltaTime, transform.position.z);
+            myRB.velocity = new Vector2(myRB.velocity.x, jumpModifier * Time.deltaTime);
+            //Tomorrow make independent jump height variable
+            Debug.Log("Space");
+        }
+
+        if (Input.GetKey(KeyCode.None))
+        {
+            myRB.velocity = new Vector2(0, 0);
+        }
     }
 }
